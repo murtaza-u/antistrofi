656708197122f6045096d8620c2162662f9c073c
Murtaza Udaipurwala <murtaza@murtazau.xyz>
2022-12-19 19:21:22 +0530

    New package: `token` to manage paseto tokens
    Implements methods to create, encrypt decrypt and refresh token, extract
    footer from the token and check if the token has expired.

---
+++ token/token.go
@@ -0,0 +1,128 @@
+package token
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"aidanwoods.dev/go-paseto"
+)
+
+var (
+	// DefaultExpiry is the default token expiration time if nothing is
+	// provided.
+	DefaultExpiry = time.Now().Add(time.Minute * 15)
+
+	ErrInvalidNB        = errors.New("expiry cannot be shorter than NB")
+	ErrMissingBody      = errors.New("missing body in params")
+	ErrMissingSecretKey = errors.New("missing secret key in params")
+
+	protocol = paseto.V4Local
+)
+
+// Params contains parameters to construct a paseto token.
+type Params struct {
+	Expiry    time.Time
+	NotBefore time.Time
+	Issuer    string
+	Audience  string
+	Body      map[string]any
+	Footer    string
+}
+
+func (p *Params) validate() error {
+	if p.Expiry.IsZero() {
+		p.Expiry = DefaultExpiry
+	}
+
+	if p.Expiry.Sub(p.NotBefore) < 0 {
+		return ErrInvalidNB
+	}
+
+	if p.Body == nil || len(p.Body) == 0 {
+		return ErrMissingBody
+	}
+
+	return nil
+}
+
+type token struct {
+	paseto.Token
+}
+
+// NewToken returns a new paseto token from the provided parameters.
+func NewToken(p Params) (*token, error) {
+	err := p.validate()
+	if err != nil {
+		return nil, fmt.Errorf(
+			"failed to validate token: %s", err.Error(),
+		)
+	}
+
+	t := new(token)
+	t.Token = paseto.NewToken()
+	t.SetExpiration(p.Expiry)
+	t.SetNotBefore(p.NotBefore)
+	t.SetIssuer(p.Issuer)
+	t.SetAudience(p.Audience)
+	t.SetFooter([]byte(p.Footer))
+
+	for k, v := range p.Body {
+		err := t.Set(k, v)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return t, nil
+}
+
+// Encrypt encrypts and signs the token. The resulting encoded string
+// can be given to the client.
+// Note: the footer of the token is never encrypted.
+func (t *token) Encrypt(secret, implict []byte) (string, error) {
+	key, err := paseto.V4SymmetricKeyFromBytes(secret)
+	if err != nil {
+		return "", fmt.Errorf(
+			"failed to create symmetric key from provided secret: %s",
+			err.Error(),
+		)
+	}
+
+	return t.V4Encrypt(key, implict), nil
+}
+
+// IsExpired checks whether the token has expired. It assumes that the
+// "exp" field is a valid RFC3339 compliant time.
+func (t *token) IsExpired() bool {
+	exp, _ := t.GetExpiration()
+	return exp.Before(time.Now())
+}
+
+// Refresh refreshes the expired token by updating the expiration to
+// (exp - iat)
+func (t *token) Refresh() error {
+	exp, err := t.GetExpiration()
+	if err != nil {
+		return fmt.Errorf(
+			"failed to get exp from token: %s",
+			err.Error(),
+		)
+	}
+
+	if !t.IsExpired() {
+		return fmt.Errorf("token has not yet expired")
+	}
+
+	iat, err := t.GetIssuedAt()
+	if err != nil {
+		return fmt.Errorf(
+			"failed to get iat from token: %s",
+			err.Error(),
+		)
+	}
+
+	t.SetIssuedAt(time.Now())
+	t.SetExpiration(time.Now().Add(exp.Sub(iat)))
+	return nil
+}
